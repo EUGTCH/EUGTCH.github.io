5ed4be494db53e47f7c1f21b3e1c25a6c08511d7
EUGTCH <165989385+EUGTCH@users.noreply.github.com>
2024-05-02 12:17:14 +0300

    save

--- js-rocket/rocket.js
+++ js-rocket/rocket.js
@@ -1,7 +1,11 @@
+var timer = null
+
 var changeState = function (state){
     document.body.className = 'body-state'+
     state;
-    if (state == 2) {};
-    document.getElementsById('cowntdown').
-innerHTML
+    if (state == 2) {
+        timer = setInterval(function(){
+            document.getElementsById('cowntdown').innerHTML = 9;
+        }, 1000);
+};
 }
